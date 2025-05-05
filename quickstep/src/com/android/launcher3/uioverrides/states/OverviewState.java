cc7e107eedbb58f00a46629dc42fbbbdbfa8fecf
Martin J. Pollard <martin0566@gmail.com>
2025-05-05 10:25:32 -0400

    Revert "Launcher3: Show AOSP clear all button when chip disabled"
    This reverts commit 5fb8ade322df8b9d1527a90e0a4782f24439bfe8.

--- quickstep/src/com/android/launcher3/uioverrides/states/OverviewState.java
+++ quickstep/src/com/android/launcher3/uioverrides/states/OverviewState.java
@@ -115,7 +115,6 @@ public class OverviewState extends LauncherState {
     @Override
     public int getVisibleElements(Launcher launcher) {
         boolean memInfo = LauncherPrefs.getPrefs(launcher).getBoolean("pref_recents_meminfo", false);
-        boolean clearAll = LauncherPrefs.getPrefs(launcher).getBoolean("pref_recents_clear_all", true);
         int elements = OVERVIEW_ACTIONS;
         DeviceProfile dp = launcher.getDeviceProfile();
         boolean showFloatingSearch;
@@ -128,9 +127,6 @@ public class OverviewState extends LauncherState {
         }
         if (showFloatingSearch) {
             elements |= FLOATING_SEARCH_BAR;
-        }
-        if (!clearAll) {
-            elements |= CLEAR_ALL_BUTTON;
         }
         if (enableSplitContextual() && launcher.isSplitSelectionActive()) {
             elements &= ~CLEAR_ALL_BUTTON;
