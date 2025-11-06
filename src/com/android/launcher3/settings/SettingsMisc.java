ea920855c59e739cd17a60cd4e9e8d76a0a3eae8
Martin J. Pollard <martin0566@gmail.com>
2025-11-06 11:49:59 -0500

    Launcher3: Add resource to enable rotation by default
    Change-Id: If9642292c58978cd6071f39ceaf3066234eb2d1e
    Signed-off-by: AnierinB <anierin@evolution-x.org>

--- src/com/android/launcher3/settings/SettingsMisc.java
+++ src/com/android/launcher3/settings/SettingsMisc.java
@@ -339,7 +339,9 @@ public class SettingsMisc extends CollapsingToolbarBaseActivity
                         return false;
                     }
                     // Initialize the UI once
-                    preference.setDefaultValue(RotationHelper.getAllowRotationDefaultValue(info));
+                    preference.setDefaultValue(
+                        RotationHelper.getAllowRotationDefaultValue(getContext(), info)
+                    );
                     return true;
                 case DEVELOPER_OPTIONS_KEY:
                     if (IS_STUDIO_BUILD) {
