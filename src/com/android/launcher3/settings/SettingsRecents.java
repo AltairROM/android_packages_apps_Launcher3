c57f89c83d2741ed0c704d29332b8df52cdbc18d
Pranav Vashi <neobuddy89@gmail.com>
2026-05-24 21:22:18 +0530

    fixup! Launcher3: Remove recents view animation styles on tablets
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/android/launcher3/settings/SettingsRecents.java
+++ src/com/android/launcher3/settings/SettingsRecents.java
@@ -190,12 +190,7 @@ public class SettingsRecents extends CollapsingToolbarBaseActivity
             setPreferencesFromResource(R.xml.launcher_recents_preferences, rootKey);
 
             PreferenceScreen screen = getPreferenceScreen();
-            for (int i = screen.getPreferenceCount() - 1; i >= 0; i--) {
-                Preference preference = screen.getPreference(i);
-                if (!initPreference(preference)) {
-                    screen.removePreference(preference);
-                }
-            }
+            removeUnsupportedPreferences(screen);
 
             // If the target preference is not in the current preference screen, find the parent
             // preference screen that contains the target preference and set it as the preference
@@ -215,6 +210,17 @@ public class SettingsRecents extends CollapsingToolbarBaseActivity
 
             if (getActivity() != null && !TextUtils.isEmpty(getPreferenceScreen().getTitle())) {
                 getActivity().setTitle(getPreferenceScreen().getTitle());
+            }
+        }
+
+        private void removeUnsupportedPreferences(PreferenceGroup group) {
+            for (int i = group.getPreferenceCount() - 1; i >= 0; i--) {
+                Preference preference = group.getPreference(i);
+                if (!initPreference(preference)) {
+                    group.removePreference(preference);
+                } else if (preference instanceof PreferenceGroup) {
+                    removeUnsupportedPreferences((PreferenceGroup) preference);
+                }
             }
         }
 
